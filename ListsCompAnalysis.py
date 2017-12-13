18dceb01bf030c115dd4cec4b06597dd96452c92
Sachin PC <pathiancherumanal@gmail.com>
2017-12-13 21:23:52 +0530

    Data Structures
    Data structures and algorithms implementation in python.

---
+++ ListsCompAnalysis.py
@@ -0,0 +1,32 @@
+'''We use python timeit module which help
+Python developers to make cross-platform timing measurements by running functions in a consistent environment and
+using timing mechanisms that are as similar as possible
+across operating systems.
+'''
+import timeit
+
+def test1():
+    l = []
+    for i in range(1000):
+        l = l + [i]
+
+def test2():
+    l = []
+    for i in range(1000):
+        l.append(i)
+
+def test3():
+    l = [i for i in range(1000)]
+
+def test4():
+    l = list(range(1000))
+
+
+t1 = timeit.Timer("test1()", "from __main__ import test1")
+print("concat ",t1.timeit(number=1000), "milliseconds")
+t2 = timeit.Timer("test2()", "from __main__ import test2")
+print("append ",t2.timeit(number=1000), "milliseconds")
+t3 = timeit.Timer("test3()", "from __main__ import test3")
+print("comprehension ",t3.timeit(number=1000), "milliseconds")
+t4 = timeit.Timer("test4()", "from __main__ import test4")
+print("list range ",t4.timeit(number=1000), "milliseconds")
