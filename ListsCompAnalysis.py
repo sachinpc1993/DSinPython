03c346e8b24d0c95249546ad1d80925c638ecaed
Sachin PC <pathiancherumanal@gmail.com>
2017-12-14 13:42:18 +0530

    List pop analysis

--- ListsCompAnalysis.py
+++ ListsCompAnalysis.py
@@ -21,7 +21,6 @@ def test3():
 def test4():
     l = list(range(1000))
 
-
 t1 = timeit.Timer("test1()", "from __main__ import test1")
 print("concat ",t1.timeit(number=1000), "milliseconds")
 t2 = timeit.Timer("test2()", "from __main__ import test2")
@@ -30,3 +29,35 @@ t3 = timeit.Timer("test3()", "from __main__ import test3")
 print("comprehension ",t3.timeit(number=1000), "milliseconds")
 t4 = timeit.Timer("test4()", "from __main__ import test4")
 print("list range ",t4.timeit(number=1000), "milliseconds")
+
+
+#Popping from end
+def test5():
+    x.pop()
+
+#Popping from index
+def test6():
+    x.pop(8)
+
+#Popping from beginning
+def test7():
+    x.pop(0)
+
+t5 = timeit.Timer("test5()","from __main__ import test5")
+t6 = timeit.Timer("test6()","from __main__ import test6")
+t7 = timeit.Timer("test7()","from __main__ import test7")
+
+#print "    PopBeg, PopEnd, PopInd"
+print "---------------------------------------------------------------------------------------------"
+print "     " + "i" + "        " + "PopBeg" + "            " + "PopEnd" + "             " + "PopInd"
+print "---------------------------------------------------------------------------------------------"
+for i in range(1000000,100000001,1000000):
+    x = list(range(i))
+    pt = t5.timeit(number=1000)
+    x = list(range(i))
+    pz = t7.timeit(number=1000)
+    x = list(range(i))
+    pi = t6.timeit(number=1000)
+
+    print str(i) + "   " + str(pz) + "   " + str(pt) + "   " + str(pi)
+    #print("%d, %15.5f, %15.5f, %15.5f" %(i,pz,pt,pi))
